e119b1b9b7b990f12ea106810a486f0f389fde7f
Blakeman11 <joshua.blakeman1@gmail.com>
2025-06-20 20:27:24 -0400

    Fix: use quantity instead of available in market API

--- src/app/api/admin/delete-market/[id]/route.ts
+++ src/app/api/admin/delete-market/[id]/route.ts
@@ -4,9 +4,12 @@ import type { NextRequest } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
-    const cardId = parseInt(context.params.id);
+    const cardId = parseInt(params.id);
 
     const deleted = await prisma.marketCard.delete({
       where: { id: cardId },
